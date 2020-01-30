4919438e10a43dae9b9645c4cdc863d7e030818f
yangchuanan <yang_an1991@163.com>
2020-01-30 21:50:33 +0800

    Rxjava test learning code submit!

--- src/main/java/rxjava/test/RxjavaTest0.java
+++ src/main/java/rxjava/test/RxjavaTest0.java
@@ -1,9 +1,14 @@
 package rxjava.test;
 
 import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.schedulers.TestScheduler;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author Chuanan YANG
@@ -32,8 +37,46 @@ public class RxjavaTest0 {
     }
 
     @Test
-    public void Test1(){
+    public void Test1() {
+        Observable<Integer> a = Observable.defer(() ->
+                Observable.just(123)
+        );
+        System.out.println("Hello defer()!");
+        a.subscribe(System.out::println);
 
+        // 创建空的
+        Observable empty = Observable.empty();
+        // 创建不发送任何数据
+        Observable empty1 = Observable.never();
+    }
+
+    @Test
+    public void Test2() {
+        Observable<String> abc = Observable.fromArray("a", "b", "c");
+        abc.subscribe(System.out::println);
+    }
+
+    @Test
+    public void Test3() {
+        Integer x = 5;
+        Observable.just("a").repeatUntil(() -> x > 4).subscribe(System.out::println);
+    }
+
+    @Test
+    public void Test4() {
+        Observable.range(0, 10).buffer(6).subscribe(System.out::println);
+    }
+
+    @Test
+    public void Test5(){
+        TestScheduler scheduler = new TestScheduler();
+        Observable<String> abc=Observable.fromArray("a","b","c");
+        abc.concatMap(x-> Observable.just(x+"c"))
+                .delay(new Random().nextInt(5), TimeUnit.SECONDS,scheduler)
+                .toList()
+                .subscribe(System.out::println,System.out::println);
+
+        scheduler.advanceTimeBy(30,TimeUnit.SECONDS);
     }
 
 }
